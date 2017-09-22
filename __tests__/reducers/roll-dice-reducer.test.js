9483a545429228e208900771760bb5f187e193a9
jacob ruleaux <jakeruleaux@gmail.com>
2017-09-22 16:26:13 -0700

    second test for dice roll fails

--- __tests__/reducers/roll-dice-reducer.test.js
+++ __tests__/reducers/roll-dice-reducer.test.js
@@ -4,11 +4,23 @@ import rollDice from './../../src/reducers/roll-dice-reducer';
 describe('Roll dice reducer', () => {
   let action;
   const rollTotal = {
-    diceRoll : "1"
+    die1 : "1",
+    die2 : "1"
   };
 
   test('should return change in state if action does not occur', () => {
     action ={type: null};
     expect(rollDice([], action)).toEqual([]);
+  });
+
+  test('should add roll to score', () => {
+    const { die1, die2 } = rollTotal;
+    action = {
+      type: c.ROLL_DICE,
+      die1: die1,
+      die2: die2
+    };
+    const diceAdded = [ rollTotal ];
+    expect(rollDice([], action)).toEqual([rollTotal]);
   });
 });
