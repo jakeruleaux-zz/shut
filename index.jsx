4b2e7e7180084efd9fbccb598c62d3890e4da81b
jacob ruleaux <jakeruleaux@gmail.com>
2017-09-22 08:52:14 -0700

    buils app comp

---
+++ index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './components/App';
+import { createStore} from 'redux';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+import { combineReducers } from 'redux';
+
+const reducer = combineReducers({})
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter>
+     <App />
+   </HashRouter>
+  </Provider>,
+  document.getElementById('react-app-root')
+);
---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <p>
+        hi
+      </p>
+    </div>
+  );
+}
+
+export default App;
