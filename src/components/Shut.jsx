16c036b79f3ece3875f9af779f8ddd3dc605d2ef
jacob ruleaux <jakeruleaux@gmail.com>
2017-09-22 09:23:44 -0700

    begin build of instructions comp

--- src/components/Shut.jsx
+++ src/components/Shut.jsx
@@ -1,13 +1,21 @@
-import React from "react";
-
+import React from 'react';
+import NewPlayer from './NewPlayer';
 import {connect} from 'react-redux';
+import Player from "./Player";
 
 class Shut extends React.Component {
 
   constructor(props) {
     super(props);
-    
-  }
+    }
+
+    render() {
+      return (
+        <div>
+          <NewPlayer/>
+        </div>
+      )
+    }
 }
 
 export default Shut;
