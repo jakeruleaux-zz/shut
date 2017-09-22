72566a09d35c5811d735c50854a3e88b275e7623
jacob ruleaux <jakeruleaux@gmail.com>
2017-09-22 08:57:24 -0700

    build header comp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
+import Header from "./Header";
 
 function App() {
   return (
     <div>
+        <Header/>
       <p>
         hi
       </p>
