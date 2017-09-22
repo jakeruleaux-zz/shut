bfb11bcc41364396c40e043c68b1ce1a948e9303
jacob ruleaux <jakeruleaux@gmail.com>
2017-09-22 09:28:50 -0700

    begin player reducer comp

---
+++ src/reducers/new-player-reducers.js
@@ -0,0 +1,15 @@
+export default (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_NAME':
+      const { name } = action;
+      return [
+        ...state,
+        {
+          name : name,
+          id : id
+        }
+      ]
+      default:
+        return state;
+  }
+}
